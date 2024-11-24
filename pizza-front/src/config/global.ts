f4ec86aa7fe5b765b34b3199af1fe123a82d3816
Duke04 <zverwf@gmail.com>
2024-11-24 18:58:31 +0500

    Добавлены переменные окружения development | production

--- pizza-front/src/config/global.ts
+++ pizza-front/src/config/global.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'http://46.146.232.223/api/'; // 46.146.232.223 localhost:31112
+export const BASE_URL = `http://${
+  process.env.REACT_APP_BUILD_MODE === 'development' ? 'localhost:31112' : '46.146.232.223'
+}/api/`;
